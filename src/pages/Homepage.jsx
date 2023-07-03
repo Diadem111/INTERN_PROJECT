a2e3a28e96bc8e7992368b89ae5317297e252ce7
wahab omodolapo <dolwithneybright1@gmail.com>
2023-07-03 19:07:07 +0100

    working on the Navbar

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styles from '../style';
+import { Navbar, Hero, Footer } from '../components';
+function Homepage  () {
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.bo}`}>
+        <Navbar/>
+        </div>
+    </div>
+
+    {/* hero section */}
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+           Hero
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+           business
+           deals
+           client 
+           footer
+      </div>
+    </div>
+
+
+  </div>
+)
+}
+
+export default Homepage;
