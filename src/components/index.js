c47dbc129fa5bff18fb491f1078507e89b378269
Marnin-A <msmaudu@gmail.com>
2023-07-15 14:44:16 +0100

    Fix routing error

--- src/components/index.js
+++ src/components/index.js
@@ -1,27 +1,18 @@
 import Navbar from "./Navbar";
 import { Footer } from "./Footer";
-<<<<<<< HEAD
-import { Hero } from "./Hero";
-import Breakfast from "./Breakfast";
-import NewRestaurant from "./NewRestaurant";
-import Outdoor from "./Outdoor";
-import Cards from "./Cards";
-import {Beforefooter} from "./Beforefooter";
-export {
-    Navbar,
-    Footer,
-    Hero,
-    NewRestaurant,
-    Outdoor,
-    Breakfast,
-    Cards,
-    Beforefooter,
-}
-=======
 import { Hero } from "./home/Hero";
 import Breakfast from "./home/Breakfast";
 import NewRestaurant from "./home/NewRestaurant";
 import Outdoor from "./home/Outdoor";
 import Cards from "./home/Cards";
-export { Navbar, Footer, Hero, NewRestaurant, Outdoor, Breakfast, Cards };
->>>>>>> ca7ce0c675ba8c1fa8c319c3e9a0d47ddbe6bcba
+import { Beforefooter } from "./Beforefooter";
+export {
+  Navbar,
+  Footer,
+  Hero,
+  NewRestaurant,
+  Outdoor,
+  Breakfast,
+  Cards,
+  Beforefooter,
+};
