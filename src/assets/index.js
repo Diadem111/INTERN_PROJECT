132abfaa135091643c9f6de040eb5bab03c4f4c9
Marnin-A <msmaudu@gmail.com>
2023-07-15 22:37:22 +0100

    Fix error

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,10 +1,22 @@
 import logo from "./logo.svg";
 import menu from "./menu.svg";
 import close from "./close.svg";
+import airbnb from "./airbnb.png";
+import send from "./Send.svg";
+import star from "./Star.svg";
+import design from "./design.png";
+import twitter from "./twitter.svg";
+import facebook from "./facebook.svg";
+import coinbase from "./coinbase.png";
+import binance from "./binance.png";
 import people01 from "./people01.png";
 import people02 from "./people02.png";
 import people03 from "./people03.png";
+import dropbox from "./dropbox.png";
+import instagram from "./instagram.svg";
+import linkedin from "./linkedin.svg";
 import cocoAsian from "./coco-asian.png";
+import shield from "./Shield.svg";
 import cutlery from "./cutlery.svg";
 import cash from "./cash_icon.svg";
 import magnifyingGlass from "./fit_zoom_icon.svg";
@@ -16,11 +28,23 @@ import spaghetti from "./spaghetti-pasta.png";
 export {
   logo,
   menu,
+  send,
   close,
+  shield,
+  design,
+  star,
+  airbnb,
+  twitter,
+  binance,
+  dropbox,
+  facebook,
+  linkedin,
+  instagram,
   people01,
   people02,
   people03,
   cocoAsian,
+  coinbase,
   cutlery,
   cash,
   magnifyingGlass,
--- src/components/index.js
+++ src/components/index.js
@@ -5,19 +5,6 @@ import Breakfast from "./home/Breakfast";
 import NewRestaurant from "./home/NewRestaurant";
 import Outdoor from "./home/Outdoor";
 import Cards from "./home/Cards";
-<<<<<<< HEAD
-import {Beforefooter} from "./Beforefooter";
-export {
-    Navbar,
-    Footer,
-    Hero,
-    NewRestaurant,
-    Outdoor,
-    Breakfast,
-    Cards,
-    Beforefooter,
-}
-=======
 import { Beforefooter } from "./Beforefooter";
 export {
   Navbar,
@@ -29,4 +16,3 @@ export {
   Cards,
   Beforefooter,
 };
->>>>>>> c47dbc129fa5bff18fb491f1078507e89b378269
