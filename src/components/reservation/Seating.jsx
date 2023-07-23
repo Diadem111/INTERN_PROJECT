1be4c339cc3f2d6efd2013dbc2b27fb2645f78fd
Marnin-A <msmaudu@gmail.com>
2023-07-23 16:49:21 +0100

    fix: remove onclick function showing in reservation button

--- src/components/reservation/Seating.jsx
+++ src/components/reservation/Seating.jsx
@@ -6,13 +6,12 @@ export default function Seating() {
   return (
     <div className="border border-[#010101] p-6 flex justify-between items-center rounded-lg mb-[5%]">
       Seating Preference{" "}
-      <button className=" bg-coreYellow py-2 border-none rounded-md"  onClick={() => {
-                navigate("/reservation/arrangement");
-              }}>
-  
-        onClick={() => navigate("arrangement")}
+      <button
         className=" bg-coreYellow py-2 border-none rounded-md"
-      
+        onClick={() => {
+          navigate("/reservation/arrangement");
+        }}
+      >
         Choose your Seat
       </button>
     </div>
