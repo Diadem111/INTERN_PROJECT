a1c12c64e345e2aae745e353ad9eb3a08022ac9f
Marnin-A <msmaudu@gmail.com>
2023-07-23 19:39:36 +0100

    Change first restaurant's name to Circa.Nonparceil, fix restaurant page responsiveness

--- src/components/restaurant/MenuCard.jsx
+++ src/components/restaurant/MenuCard.jsx
@@ -12,34 +12,41 @@ export default function MenuCard({
   const navigate = useNavigate();
   return (
     <div className="grid grid-cols-10 p-5 border border-[#64748B] my-12">
-      <div className="flex flex-col col-start-1 col-span-5 gap-3">
-        <h3 className="text-xl font-bold">{heading}</h3>
-        <div className="grid grid-cols-8 col-start-3 items-center p-1">
-          <span className="grid col-start-1 justify-start">
-            <img className="h-full w-11/12" src={cash} alt="Cash" />
-          </span>
-          <span className=" col-start-2 col-span-5 font-medium">
-            NGN {price} per person
-          </span>
-        </div>
-        <div className="grid grid-cols-8 col-start-3 items-center p-1">
-          <span className="grid col-start-1 justify-start">
-            <img className="h-full w-11/12" src={calender} alt="Calender" />
-          </span>
-          <span className=" col-start-2 col-span-5 font-medium">
-            {dateRange}
-          </span>
+      <div className="flex flex-col col-start-1 mds:col-span-5 col-span-10 gap-3  ">
+        <div className="flex">
+          <div>
+            <h3 className="text-xl font-bold mb-2">{heading}</h3>
+            <div className="flex gap-9 col-start-3 items-center p-1">
+              <span className="grid col-start-1 justify-start">
+                <img className="h-full w-11/12" src={cash} alt="Cash" />
+              </span>
+              <span className=" col-start-2 col-span-5 font-medium">
+                NGN {price} per person
+              </span>
+            </div>
+            <div className="flex gap-9 col-start-3 items-center p-1">
+              <span className="grid col-start-1 justify-start">
+                <img className="h-full w-11/12" src={calender} alt="Calender" />
+              </span>
+              <span className=" col-start-2 col-span-5 font-medium">
+                {dateRange}
+              </span>
+            </div>
+          </div>
+          <div className="mds:hidden flex justify-center">
+            <img className=" w-3/5 rounded-md" src={img} alt="Image of food" />
+          </div>
         </div>
         <p className="my-4">{foodDescription}</p>
         <button
           onClick={() => navigate("reservation")}
           id="but"
-          className="w-2/5"
+          className="w-2/5 mds:self-start self-center"
         >
           Book a Table
         </button>
       </div>
-      <div className="col-start-7 col-span-4">
+      <div className="col-start-7 col-span-4 mds:inline hidden">
         <img
           className="h-full w-11/12 rounded-md"
           src={img}
--- tailwind.config.js
+++ tailwind.config.js
@@ -22,6 +22,7 @@ module.exports = {
       xs: "480px",
       ss: "620px",
       sm: "768px",
+      mds: "900px",
       md: "1060px",
       lg: "1200px",
       xl: "1700px",
