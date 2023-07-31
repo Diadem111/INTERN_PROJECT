5283cb33e2a0decb3483ebb7eb14e9addcfdbba5
Marnin-A <msmaudu@gmail.com>
2023-07-31 09:05:58 +0100

    feat: adds smooth scroll on navigate

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { HashRouter } from "react-router-dom";
+import SmoothScroll from "./components/SmoothScroll";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
@@ -7,7 +8,9 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HashRouter>
-      <App />
+      <SmoothScroll>
+        <App />
+      </SmoothScroll>
     </HashRouter>
   </React.StrictMode>
 );
